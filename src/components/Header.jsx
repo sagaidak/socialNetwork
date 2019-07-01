abde9123970888816f0d434cda0a12aa300f6e22
Alexandr <sagaidak1337@gmail.com>
2019-07-01 16:53:18 +0300

    project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://i.imgur.com/GlAMewm.jpg' className='img'/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://i.redd.it/wkk4zz08qf731.jpg' />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My Posts
+                <div>
+                    New Posts
+                </div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;

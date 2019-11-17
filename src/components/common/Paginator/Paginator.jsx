5055c11b9e7b5459207031c3e96780ccb91bf226
Alexandr <sagaidak1337@gmail.com>
2019-11-17 21:36:21 +0200

    new paginator

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Paginator.module.css';
 
-const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged }) => {
+const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged  }) => {
 
     let pagesCount = Math.ceil(totalUsersCount / pageSize);
 
@@ -11,15 +11,30 @@ const Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged }) => 
         pages.push(i);
     }
 
-    return (
-        <div>
-            {pages.map(p => {
+    let portionSize = 10;
+
+    let portionCount = Math.ceil(pagesCount / portionSize);
+    let [portionNumber, setPortionNumber] = useState(1);
+    let leftBorder = (portionNumber - 1) * portionSize + 1;
+    let rightBorder  = leftBorder + portionSize;
+
+    return (<div>
+            { portionNumber > 1 &&
+                <button onClick={() => { setPortionNumber(portionNumber-1)}}>left</button> }
+
+            {pages
+                .filter(p => p>=leftBorder && p<=rightBorder)
+                .map(p => {
                 return <span className={currentPage === p ? s.selectedPage : ''}
                              onClick={() => {
                                  onPageChanged(p)
                              }}
                 >{p}</span>
             })}
+
+
+            { portionCount > portionNumber &&
+                <button onClick={() => { setPortionNumber(portionNumber+1)}}>right</button> }
 
         </div>
     )
